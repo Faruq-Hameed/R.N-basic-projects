21256c49954b710c0e83fd0581a4d923f59ec749
Faruq-Hameed <faruqhameed1@gmail.com>
2025-01-27 21:57:18 +0100

    added ResloveScreen

--- track-app/src/screens/ResolveScreen.js
+++ track-app/src/screens/ResolveScreen.js
@@ -27,7 +27,7 @@ const ResolveScreen = ({ onResolve }) => {
     loadTokenFromStorage();
   }, []);
   if (loading) {
-    return ( //if loading, this activity indicator
+    return ( //if loading, this activity indicator wil
       <ActivityIndicator
         size="large"
         color="#0000ff"
